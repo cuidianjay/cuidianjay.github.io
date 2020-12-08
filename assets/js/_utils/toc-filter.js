872918b88d8a19c0643210e6edf2855821b93ee5
cuidianjay <cuidianjay@163.com>
2020-12-09 00:19:19 +0800

    右侧文章导航

--- assets/js/_utils/toc-filter.js
+++ assets/js/_utils/toc-filter.js
@@ -8,7 +8,9 @@
 
 $(function() {
   if ($("#post-wrapper .post-content h1").length === 0
-      && $("#post-wrapper .post-content h2").length === 0) {
+      && $("#post-wrapper .post-content h2").length === 0
+	  && $("#post-wrapper .post-content h3").length === 0
+	  && $("#post-wrapper .post-content h4").length === 0) {
     $("#toc-wrapper").addClass("unloaded");
   }
 });
